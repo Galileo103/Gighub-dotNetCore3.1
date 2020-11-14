cb50163e92aa5804067ca355601c39661e65e121
Galal Shaban <galal.shaban@gameball.co>
2020-11-14 15:58:01 +0200

    Add Who I Follow

---
+++ Gighub/Controllers/FolloweesController.cs
@@ -0,0 +1,35 @@
+﻿using Gighub.Data;
+using Gighub.Models.Entities;
+using Gighub.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gighub.Controllers
+{
+    //[Route("api/v{api-version:apiVersion}/[controller]")]
+    //[ApiController]
+    public class FolloweesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public FolloweesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var artists = _context.Followings
+                .Where(a => a.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
